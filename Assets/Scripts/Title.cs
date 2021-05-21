cfd5f1555a5823be93f413afaf819c2e841b26d2
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-05-21 11:11:55 +0900

    新規作成

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+    // スタートボタンが押されると呼ばれる
+    public void PressStart()
+    {
+
+        if (!firstPush)
+        {
+            
+            //次のシーンに行く命令を書く
+
+            firstPush = true;
+        }
+    }
+  
+}
